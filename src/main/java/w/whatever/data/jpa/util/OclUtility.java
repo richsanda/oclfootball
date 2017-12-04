b58949dcd95d3ba772567b297be84c6c83e02232
rich <richsanda@yahoo.com>
2017-12-04 08:38:40 -0500

    top 20 per team

--- src/main/java/w/whatever/data/jpa/util/OclUtility.java
+++ src/main/java/w/whatever/data/jpa/util/OclUtility.java
@@ -7,5 +7,5 @@ public class OclUtility {
 
     public static final Integer currentSeason = 2017;
     public static final Integer currentScoringPeriod = 13;
-    public static final Integer countPerTeam = 1000;
+    public static final Integer countPerTeam = 20;
 }
